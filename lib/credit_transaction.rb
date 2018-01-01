8e46bace8aa1f7de7608bbae33d7dd94edc0e4cb
Lewis <lewisyoul@gmail.com>
2018-01-01 21:00:32 +0000

    add balance to credit transaction

--- lib/credit_transaction.rb
+++ lib/credit_transaction.rb
@@ -2,10 +2,11 @@ require 'date'
 
 class CreditTransaction
 
-  attr_reader :credit, :date
+  attr_reader :credit, :date, :balance
 
-  def initialize(amount, date = Date.today.to_s)
+  def initialize(amount, date = Date.today.to_s, balance)
     @credit = amount
     @date = date
+    @balance = balance
   end
 end
