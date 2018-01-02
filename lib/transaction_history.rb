70d75a2e55ca6bd903bf39195f81837ac5cb9a3d
Lewis <lewisyoul@gmail.com>
2018-01-02 11:15:03 +0000

    inject Transaction class
    TransactionHistory now receives a Transaction class upon instantiation

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -1,9 +1,20 @@
+require_relative './transaction.rb'
+
 class TransactionHistory
 
-  attr_accessor :all_transactions
+  attr_accessor :all_transactions, :transaction_class
 
-  def initialize
+  def initialize(transaction_class = Transaction)
+    @transaction_class = transaction_class
     @all_transactions = []
   end
-  
+
+  def add_funds(credit_amount, balance = 500)
+    @all_transactions << @transaction_class.new(balance, credit_amount, 0)
+  end
+
+  def remove_funds(debit_amount, balance = 500)
+    @all_transactions << @transaction_class.new(balance, 0, debit_amount)
+  end
+
 end
--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -1,11 +1,36 @@
 require 'transaction_history'
 
 describe TransactionHistory do
-  subject { TransactionHistory.new }
+  let(:transaction_class) { double(:transaction_class) }
+  let(:credit_transaction_instance) { double(:credit_transaction_instance) }
+  let(:debit_transaction_instance) { double(:debit_transaction_instance) }
+  subject { TransactionHistory.new(transaction_class) }
+
+  describe '#transaction_class' do
+    it 'should return the Transaction class' do
+      expect(subject.transaction_class).to eq(transaction_class)
+    end
+  end
 
   describe '#all_transactions' do
     it 'should be an instance of Array' do
       expect(subject.all_transactions).to be_an_instance_of(Array)
+    end
+  end
+
+  describe '#add_funds' do
+    it 'adds the transaction to the transaction history' do
+      allow(transaction_class).to receive(:new).with(500, 500, 0).and_return(credit_transaction_instance)
+      subject.add_funds(500)
+      expect(subject.all_transactions[0]).to eq(credit_transaction_instance)
+    end
+  end
+
+  describe '#remove_funds' do
+    it 'adds the transaction to the transaction history' do
+      allow(transaction_class).to receive(:new).with(0, 0, 500).and_return(debit_transaction_instance)
+      subject.remove_funds(500, 0)
+      expect(subject.all_transactions[0]).to eq(debit_transaction_instance)
     end
   end
 
