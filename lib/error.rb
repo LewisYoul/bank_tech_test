68591d3851bf6f15fac9eedace7622e691f64746
Lewis <lewisyoul@gmail.com>
2018-01-30 11:52:11 +0000

    Implement debit checks for Error class

--- lib/error.rb
+++ lib/error.rb
@@ -1,3 +1,8 @@
 class Error
-  
+
+  def debit_checks(balance_amount, amount)
+    raise 'Please enter an Integer' unless amount.is_a?(Integer)
+    raise 'You have insufficient funds' if (balance_amount - amount) < 0
+    raise "You can't withdraw a negative" if amount < 0
+  end
 end
--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -1,5 +1,18 @@
 require 'error'
 
 describe Error do
-  
+
+  subject { Error.new }
+
+  describe "#debit_checks" do
+    it "throws an error if the amount is not an integer" do
+      expect { subject.debit_checks(200, "string") }.to raise_error('Please enter an Integer')
+    end
+    it "throws an error if the withdrawal would result in negative balance" do
+      expect { subject.debit_checks(200, 500) }.to raise_error('You have insufficient funds')
+    end
+    it "throws an error if the amount given is negative" do
+      expect { subject.debit_checks(200, -400) }.to raise_error("You can't withdraw a negative")
+    end
+  end
 end
