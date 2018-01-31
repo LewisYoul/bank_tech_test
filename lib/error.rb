19bab84dbbfbc9278c40348965157c2c343b13d1
Lewis <lewisyoul@gmail.com>
2018-01-31 09:49:47 +0000

    Rename MINIMUM to MINIMUM_BALANCE

--- lib/error.rb
+++ lib/error.rb
@@ -1,14 +1,14 @@
 class Error
-  MINIMUM = 0
+  MINIMUM_BALANCE = 0
 
   def debit_checks(balance_amount, amount)
     raise 'Please enter an Integer' unless amount.is_a?(Integer)
-    raise 'You have insufficient funds' if (balance_amount - amount) < MINIMUM
-    raise "You can't withdraw a negative" if amount < MINIMUM
+    raise 'You have insufficient funds' if (balance_amount - amount) < MINIMUM_BALANCE
+    raise "You can't withdraw a negative" if amount < MINIMUM_BALANCE
   end
 
   def credit_checks(balance_amount, amount)
     raise 'Please enter an Integer' unless amount.is_a?(Integer)
-    raise "You can't credit a negative value" if amount <= MINIMUM
+    raise "You can't credit a negative value" if amount <= MINIMUM_BALANCE
   end
 end
