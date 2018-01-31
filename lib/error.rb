e60162751912cf0774edd5ba8671fb8b1fc27fde
Lewis <lewisyoul@gmail.com>
2018-01-31 09:49:06 +0000

    Refactor minimum balance to be a constant

--- lib/error.rb
+++ lib/error.rb
@@ -1,13 +1,14 @@
 class Error
+  MINIMUM = 0
 
   def debit_checks(balance_amount, amount)
     raise 'Please enter an Integer' unless amount.is_a?(Integer)
-    raise 'You have insufficient funds' if (balance_amount - amount) < 0
-    raise "You can't withdraw a negative" if amount < 0
+    raise 'You have insufficient funds' if (balance_amount - amount) < MINIMUM
+    raise "You can't withdraw a negative" if amount < MINIMUM
   end
 
   def credit_checks(balance_amount, amount)
     raise 'Please enter an Integer' unless amount.is_a?(Integer)
-    raise "You can't credit a negative value" if amount <= 0
+    raise "You can't credit a negative value" if amount <= MINIMUM
   end
 end
